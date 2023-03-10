b3cc440aa35edf56ee8f19bbd05c5f84f8d664ba
more rushi <morerushikesh1111@gmail.com>
2023-03-10 11:51:18 +0530

    second

--- app1/views.py
+++ app1/views.py
@@ -16,9 +16,12 @@ def cc_home(request):
 
 def cc_car(request):
     if request.method =="POST":
-        form_responce = request.POST.get('abc')
+        electricity_monthly = request.POST.get('electricity_monthly')
+        distance_petrol = request.POST.get('distance_petrol')
+        distance_disel = request.POST.get('distance_disel')
+        lpg = request.POST.get('lpg')
 
-        print((form_responce))
+        print(electricity_monthly,distance_petrol,distance_disel,lpg)
         
     return render(request, 'cc_car.html')
 
@@ -32,3 +35,14 @@ def cc_output(request):
 
 def cc_industry(request):
     return render(request, 'cc_industry.html')
+
+
+
+# Electricity_yearly = Electricity_monthly*12
+# Electricity_yearly_emission = Electricity_yearly*0.85
+# Pertrol_Km = Yearly_distance_travelled_petrol/10
+# Petrol_Emission = Pertrol_Km*2.296
+# Diesel_Km = Yearly_distance_travelled_diesel/12
+# Diesel_Emission = Diesel_Km*2.653
+# LPG_yearly_Emission = LPG_qty * 2.983
+# Carbon_Footprint = (Electricity_yearly_emission + Petrol_Emission + Diesel_Emission + LPG_yearly_Emission)/1000
