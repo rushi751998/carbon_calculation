2485889a71941c2c2b5686a27d3c34edda9d3809
more rushi <morerushikesh1111@gmail.com>
2023-03-28 16:12:01 +0530

    ru

--- app1/views.py
+++ app1/views.py
@@ -9,21 +9,34 @@ def index(request):
 def cc(request):
     return render(request, 'cc.html')
 
+def family_carbon_calculator(Electricity_monthly,Yearly_distance_travelled_petrol,Yearly_distance_travelled_diesel,LPG_qty):
+    Electricity_yearly = Electricity_monthly*12
+    Electricity_yearly_emission = Electricity_yearly*0.85
+    Pertrol_Km = Yearly_distance_travelled_petrol/10
+    Petrol_Emission = Pertrol_Km*2.296
+    Diesel_Km = Yearly_distance_travelled_diesel/12
+    Diesel_Emission = Diesel_Km*2.653
+    LPG_yearly_Emission = LPG_qty * 2.983
+    Carbon_Footprint = (Electricity_yearly_emission + Petrol_Emission + Diesel_Emission + LPG_yearly_Emission)/1000
 
+    return Carbon_Footprint
+
+ls = []
 def cc_home(request):
     parm = {"form":Calculator_form()}
     return render(request, 'cc_home.html',parm)
 
 def cc_car(request):
-    if request.method =="POST":
-        electricity_monthly = request.POST.get('electricity_monthly')
-        distance_petrol = request.POST.get('distance_petrol')
-        distance_disel = request.POST.get('distance_disel')
-        lpg = request.POST.get('lpg')
+    if request.method =="POST": 
+        electricity_monthly = int(request.POST.get('electricity_monthly'))
+        Yearly_distance_travelled_petrol = int(request.POST.get('distance_petrol'))
+        Yearly_distance_travelled_diesel = int(request.POST.get('distance_disel'))
+        LPG_qty = int(request.POST.get('lpg'))
 
-        print(electricity_monthly,distance_petrol,distance_disel,lpg)
-        
-    return render(request, 'cc_car.html')
+        ls.append(family_carbon_calculator(electricity_monthly,Yearly_distance_travelled_petrol,Yearly_distance_travelled_diesel,LPG_qty))
+    param = {"calulations":round(sum(ls),2)}
+    print(param)
+    return render(request, 'cc_car.html',param)
 
 
 
